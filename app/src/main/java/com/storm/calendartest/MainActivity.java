a30b9f81f5f67a55db2277ab26fb9bf02291d4cc
iflenix <iflenix@gmail.com>
2015-06-17 18:19:09 +0300

    разобрался

--- app/src/main/java/com/storm/calendartest/MainActivity.java
+++ app/src/main/java/com/storm/calendartest/MainActivity.java
@@ -2,14 +2,20 @@ package com.storm.calendartest;
 
 import android.content.ContentProvider;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Objects;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -43,26 +49,39 @@ public class MainActivity extends ActionBarActivity {
     }
 
     private boolean isFirst = true;
+    private static Cursor calCursor = null;
 
     public void onGetEventsButtonClick (View v){
         ContentResolver cr = getContentResolver();
+        TextView tv = (TextView) findViewById(R.id.textview);
 
-        Cursor calCursor = cr.query(CalendarContract.Events.CONTENT_URI,null,null,null,null);
-        if(isFirst) {
+        if(calCursor == null)
+            calCursor = cr.query(CalendarContract.Events.CONTENT_URI,null,null,null,null);
 
-            if (calCursor.moveToFirst()) {
+
+            if (calCursor.moveToNext() && isFirst) {
+
+                tv.setText(String.valueOf(calCursor.getCount()) +" "+  calCursor.getPosition());
 
                 Toast.makeText(this, calCursor.getString(calCursor.getColumnIndexOrThrow(CalendarContract.Events.TITLE)), Toast.LENGTH_LONG).show();
                 isFirst = false;
-            }
+            }else if (!isFirst){
+            //calCursor.moveToPosition(15);
 
-        }else if (calCursor.moveToNext()){
-
+                tv.setText(calCursor.getString(calCursor.getColumnIndexOrThrow(CalendarContract.Events.TITLE))  + " позиция: " +  calCursor.getPosition());
                 Toast.makeText(this, calCursor.getString(calCursor.getColumnIndexOrThrow(CalendarContract.Events.TITLE)), Toast.LENGTH_LONG).show();
             }
+       // calCursor.close();
+    }
 
-        calCursor.close();
-
+    public void onInsertEventButtonClick(View v){
+        //Uri insertUri = Uri.withAppendedPath(CalendarContract.Events.CONTENT_URI);
+        Intent intent = new Intent(Intent.ACTION_INSERT,CalendarContract.Events.CONTENT_URI);
+        Calendar startTime = Calendar.getInstance();
+        startTime.set(2015,Calendar.JUNE,18,15,0);
+        intent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, startTime.getTimeInMillis());
+        intent.putExtra(CalendarContract.Events.TITLE,"TEst event");
+        startActivity(intent);
 
 
     }
